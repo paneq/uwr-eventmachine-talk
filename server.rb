ea8d1036156b22ac12e20d8511aa9227a27a4051
Robert Pankowecki <robert.pankowecki@gmail.com>
2013-01-28 15:13:19 +0100

    One periodic per app instead of multiple

--- server.rb
+++ server.rb
@@ -5,25 +5,30 @@ Connections = []
 class ChatServer < EM::Connection
   def post_init
     Connections << self
-    send_data(number_of_connections)
-    @timer = EM.add_periodic_timer(5) do
-      send_data(number_of_connections)
-    end
   end
 
   def unbind
-    EM.cancel_timer(@timer)
     Connections.delete(self)
+  end
+
+  def notify_about_connections(size)
+    send_data( number_of_connections(size) )
   end
 
   private
 
-  def number_of_connections
-    "# of connections: #{Connections.size}\n"
+  def number_of_connections(size)
+    "# of connections: #{size}\n"
   end
 
 end
 
 EventMachine.run {
+  timer = EM.add_periodic_timer(5) do
+    Connections.each do |c|
+      c.notify_about_connections(Connections.size)
+    end
+  end
+
   EventMachine.start_server "127.0.0.1", 8000, ChatServer
 }
