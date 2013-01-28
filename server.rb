bf9b3a96d2f22dc858483d660074cad217a8df6d
Robert Pankowecki <robert.pankowecki@gmail.com>
2013-01-28 15:03:27 +0100

    send info about number of connections

--- server.rb
+++ server.rb
@@ -5,11 +5,21 @@ Connections = []
 class ChatServer < EM::Connection
   def post_init
     Connections << self
-    send_data "# of connections: #{Connections.size}\n"
+    send_data(number_of_connections)
+    @timer = EM.add_timer(10) do
+      send_data(number_of_connections)
+    end
   end
 
   def unbind
+    EM.cancel_timer(@timer)
     Connections.delete(self)
+  end
+
+  private
+
+  def number_of_connections
+    "# of connections: #{Connections.size}\n"
   end
 
 end
